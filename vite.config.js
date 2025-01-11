a00f2679ba427740fc2b679074d27e77ce299b6a
Justyne Phan <justynephan@gmail.com>
2025-01-11 13:16:16 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Ensures relative paths for hosted assets
+  base: '/Portfolio/', // Replace 'repository-name' with your GitHub repo name
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output folder for build artifacts
-    rollupOptions: {
-      output: {
-        // Ensure proper file extensions for scripts
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-  },
 });
