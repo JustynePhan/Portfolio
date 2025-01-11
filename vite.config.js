ae61032930c145a32dd59120738b3a29641ec096
Justyne Phan <justynephan@gmail.com>
2025-01-11 13:36:09 -0500

    removal of the githun folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // Replace 'repository-name' with your GitHub repo name
   plugins: [react()],
 });
